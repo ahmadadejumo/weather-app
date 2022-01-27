acd1f44390dfb2aa5ff37429d5ff3cae09c39643
ahmadadejumo <adejumoahmad@gmail.com>
2022-01-27 22:52:46 +0100

    add scroller in background

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:flutter_cubit/misc/colors.dart';
 import 'package:flutter_cubit/widgets/app_large_text.dart';
@@ -38,6 +40,7 @@ class _WelcomePageState extends State<WelcomePage> {
               child: Container(
                 margin: const EdgeInsets.only(top: 150, left: 20, right: 20),
                 child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
@@ -64,6 +67,21 @@ class _WelcomePageState extends State<WelcomePage> {
                         ),
                         ResponsiveButton(width: 120)
                       ],
+                    ),
+                    Column(
+                      children: List.generate(3, (indexDots) {
+                        return Container(
+                          margin: const EdgeInsets.only(bottom: 2),
+                          width: 8,
+                          height: index == indexDots ? 25 : 8,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                            color: index == indexDots
+                                ? AppColors.mainColor
+                                : AppColors.mainColor.withOpacity(0.3),
+                          ),
+                        );
+                      }),
                     )
                   ],
                 ),
