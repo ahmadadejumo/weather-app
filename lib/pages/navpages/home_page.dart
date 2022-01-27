c4c3faa1157dffed325a672129cc44e1f2cc0ea4
ahmadadejumo <adejumoahmad@gmail.com>
2022-01-27 23:50:19 +0100

    add menu icon, box and text

--- lib/pages/navpages/home_page.dart
+++ lib/pages/navpages/home_page.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:flutter_cubit/widgets/app_large_text.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,6 +13,43 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // menu text
+          Container(
+            padding: const EdgeInsets.only(top: 70, left: 20),
+            child: Row(
+              // ignore: prefer_const_literals_to_create_immutables
+              children: [
+                Icon(
+                  Icons.menu,
+                  size: 30,
+                  color: Colors.black54,
+                ),
+                Expanded(child: Container()),
+                Container(
+                  margin: const EdgeInsets.only(right: 20),
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.grey.withOpacity(0.5)),
+                )
+              ],
+            ),
+          ),
+          SizedBox(height: 40),
+          // discover text
+          Container(
+            margin: const EdgeInsets.only(left: 20),
+            child: AppLargeText(text: "Discover"),
+          ),
+          SizedBox(height: 30),
+          // tabbar
+        ],
+      ),
+    );
   }
 }
--- lib/pages/navpages/main_page.dart
+++ lib/pages/navpages/main_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: deprecated_member_use, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:flutter_cubit/pages/navpages/bar_item_page.dart';
 import 'package:flutter_cubit/pages/navpages/home_page.dart';
@@ -42,6 +44,7 @@ class _MainPageState extends State<MainPage> {
         showSelectedLabels: false,
         showUnselectedLabels: false,
         elevation: 0,
+        // ignore: prefer_const_literals_to_create_immutables
         items: [
           BottomNavigationBarItem(title: Text("Home"), icon: Icon(Icons.apps)),
           BottomNavigationBarItem(
